dcb35a25d20fa3561a9d7dde09858957a7c39c05
kronosfere@gmail.com <kronosfere@gmail.com>
2017-02-27 02:11:43 +0800

    1st Commit

---
+++ New Unity Project/Assets/Scripts/TowerSelectScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSelectScript : BaseSelectableArea {
+
+	[SerializeField]
+
+
+	void OnMouseDown()
+	{
+		Debug.Log("test");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

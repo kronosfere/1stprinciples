f5e3dc972c23abbe4103c9be5ec5b0add1a9a58a
Kelvin <alcandem@gmail.com>
2017-03-12 20:10:41 +0800

    Added tower reflector, ground plane and base game typings.

---
+++ New Unity Project/Assets/Scripts/GameObjects/BaseGameObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseGameObject : MonoBehaviour {
+
+    public enum GameObjectType
+    {
+        GAMEOBJ_LIGHT_TOWER = 0,
+        GAMEOBJ_LIGHT_REFLECT,
+        GAMEOBJ_LIGHT_BEAM,
+        GAMEOBJ_TOTAL_TYPE
+    };
+
+    [SerializeField]
+    public GameObjectType GameObjType;
+    [SerializeField]
+    public uint GameObjectOwner;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New Unity Project/Assets/Scripts/GameObjects/TowerReflectors.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerReflectors : MonoBehaviour {
+
+    [SerializeField]
+    float ReflectorAngle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BaseGameObject tmpObj = collision.gameObject.GetComponent<BaseGameObject>();
+
+        // Check if the beam is the owner's
+        if (tmpObj.GameObjectOwner == this.GetComponent<BaseGameObject>().GameObjectOwner)
+        {
+            // Redirect if it is a beam, else do nothing
+            if(tmpObj.GameObjType == BaseGameObject.GameObjectType.GAMEOBJ_LIGHT_BEAM)
+            {
+                // Create a beam at reflected angle
+            }
+        }
+
+        // Take damage if projectile is not owner's
+        else
+        {
+
+        }
+    }
+}
